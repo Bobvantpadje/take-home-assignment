e043b0555a135e31ae0b80e7a3cb1bcb071bf9d8
Bob <Bob van 't Padje>
2020-12-14 11:15:54 +0100

    cleanup(recommend-car): added form validation

--- pages/components/RecommendCar/index.tsx
+++ pages/components/RecommendCar/index.tsx
@@ -1,17 +1,37 @@
+import { useFormik } from "formik";
 import React from "react";
 import { Input } from "../Input";
 import { getCarsWithTotalPrice, rankCarsByTotalPrice } from "./helperFunction";
+import * as Yup from "yup";
+
+type Params = {
+  distance: string;
+  years: string;
+  fuelPrice: string;
+};
 
 export const RecommendCar: React.FC<{ cars: Car[] }> = ({ cars }) => {
-  const [distancePerMontInKM, setDistancePerMontInKM] = React.useState("");
-  const [years, setYears] = React.useState("");
   const [rankedCars, setRankedCars] = React.useState<Car[]>([]);
-  const [fuelPrice, setFuelPrice] = React.useState('');
+  const formik = useFormik({
+    initialValues: {
+      distance: "",
+      years: "",
+      fuelPrice: "",
+    },
+    validationSchema: Yup.object().shape({
+      distance: Yup.number().required("Required").typeError("Invalid Number"),
+      years: Yup.number().required("Required").typeError("Invalid Number"),
+      fuelPrice: Yup.number().required("Required").typeError("Invalid Number"),
+    }),
+    onSubmit: (values) => {
+      calculateCarRanking(values);
+    },
+  });
 
-  const calculateCarRanking = () => {
+  const calculateCarRanking = ({ distance, years, fuelPrice }: Params) => {
     const carsWithTotalPrice = getCarsWithTotalPrice(
       cars,
-      parseFloat(distancePerMontInKM),
+      parseFloat(distance),
       parseFloat(years),
       parseFloat(fuelPrice)
     );
@@ -26,33 +46,41 @@ export const RecommendCar: React.FC<{ cars: Car[] }> = ({ cars }) => {
         Enter the amount of km you would like to drive per month and the amount
         of years and we will rank the cars based on the total price!
       </p>
-      <Input
-        label="distance(km) per month"
-        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-          setDistancePerMontInKM(e.target.value);
-        }}
-        value={distancePerMontInKM}
-      />
-      <Input
-        label="years"
-        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-          setYears(e.target.value);
-        }}
-        value={years}
-      />
-      <Input
-        label="Fuel price"
-        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-          setFuelPrice(e.target.value);
-        }}
-        value={fuelPrice}
-      />
-      <button onClick={calculateCarRanking}>rank cars</button>
+      <form onSubmit={formik.handleSubmit}>
+        <Input
+          id="distance"
+          name="distance"
+          type="text"
+          onChange={formik.handleChange}
+          value={formik.values.distance}
+          label="Distance (KM per month):"
+          error={formik.touched.distance ? formik.errors.distance : ""}
+        />
+        <Input
+          id="years"
+          name="years"
+          type="text"
+          onChange={formik.handleChange}
+          value={formik.values.years}
+          label="Years:"
+          error={formik.touched.years ? formik.errors.years : ""}
+        />
+        <Input
+          id="fuelPrice"
+          name="fuelPrice"
+          type="text"
+          onChange={formik.handleChange}
+          value={formik.values.fuelPrice}
+          label="Fuel price"
+          error={formik.touched.fuelPrice ? formik.errors.fuelPrice : ""}
+        />
+        <button type="submit">rank cars</button>
+      </form>
       <ul>
         {rankedCars.map((car, index) => (
           <li key={car.id}>
             <b>{index + 1}.</b> {car.make} {car.model} - totalPrice: €
-            {car.totalAccumulatedPrice}
+            {car.totalAccumulatedPrice.toFixed(2)}
           </li>
         ))}
       </ul>
